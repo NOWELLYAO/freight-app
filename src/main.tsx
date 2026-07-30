7b5fabec713e2f5b4df0b945b7d7b5edc5b06d5a
NOWELLYAO <nowellyao@yahoo.fr>
2026-07-30 18:10:21 +0000

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import FreightCalculator from "./FreightCalculator";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <FreightCalculator />
+    <App />
   </React.StrictMode>
 );
